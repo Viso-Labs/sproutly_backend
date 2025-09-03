d037a9d235335fadeed59b2631702f70ad3d2d33
unknown <kalistan01@gmail.com>
2025-09-03 15:36:11 +0530

    commit

--- server.js
+++ server.js
@@ -94,5 +94,5 @@ app.get("/og/u/:signedId.png", async (req, res) => {
 app.get("/", (req, res) => {
   res.status(200).send("API is running.");
 });
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5008;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
